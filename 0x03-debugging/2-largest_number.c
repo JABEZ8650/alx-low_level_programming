af5c6becc1ce384432e47c53b4e962f8f72fd24c
yabets muhaba <eyumuhaba@gmail.com>
2022-06-12 20:54:00 -0700

    new addition

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -6,6 +6,7 @@
  * @c: third integer
  * Return: largest number
  */
+
 int largest_number(int a, int b, int c)
 {
 	int largest;
@@ -21,5 +22,6 @@ int largest_number(int a, int b, int c)
 	{
 		largest = c;
 	}
+
 	return (largest);
 }
