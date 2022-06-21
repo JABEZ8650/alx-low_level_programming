ea17a7f48a181d1d0c6b438749be21b3d9bdaffb
yabets muhaba <eyumuhaba@gmail.com>
2022-06-20 20:19:38 -0700

    new addition

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -2,7 +2,6 @@
 
 int main(void)
 {
-	
 	int n;
 	int a[5];
 	int *p;
@@ -20,6 +19,5 @@ int main(void)
 	p[5] = 98;
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
-	
 	return (0);
 }
