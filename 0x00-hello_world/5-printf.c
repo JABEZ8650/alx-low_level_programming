75165c1620783ac187ddc1adf5694b22573e538c
yabets muhaba <eyumuhaba@gmail.com>
2022-07-21 08:36:09 -0700

    new commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,8 +5,8 @@
  *
  * Return: Returns zero
  */
-int main(void)
-{
-	        printf("with proper grammar, but the outcome is a piece of art,\n");
-	        return 0;
+int main(void){
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
 }
